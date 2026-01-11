30fe1af02d0390ec36c7b2914987236efd3fb947
Samuobe <samuobe@ik.me>
2026-01-11 21:39:03 +0100

    Added telnete control to cd and web control to telnet

--- function/cd.py
+++ function/cd.py
@@ -1,12 +1,29 @@
 import os
+import threading
+import time
 
 def start_cd():
     os.system("killall vlc")
-    os.system('cvlc --intf http cdda:///dev/sr0 --http-password "ciao"')
+    time.sleep(0.3)
+    os.system(
+        'cvlc cdda:///dev/sr0 '
+        '--intf http '
+        '--http-password "ciao" '
+        '--extraintf telnet '
+        '--telnet-password ciao '
+        '--telnet-port 4212'
+    )
 
 def stop_cd():
-    import threading
     def kill():
         os.system("killall vlc")
-        os.system("cvlc -I telnet --telnet-password=ciao --telnet-port=4212")
+        time.sleep(0.3)
+        os.system(
+            'cvlc '
+            '--intf http '
+            '--http-password "ciao" '
+            '--extraintf telnet '
+            '--telnet-password ciao '
+            '--telnet-port 4212'
+        )
     threading.Thread(target=kill, daemon=True).start()
